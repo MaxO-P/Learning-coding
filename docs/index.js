b0834b5ea3eb3dfbdfb67691b5d8a9111b8e5ff9
Mistakd <56619664+RealMistakd@users.noreply.github.com>
2021-09-29 09:58:34 -0400

    notes

--- docs/index.js
+++ docs/index.js
@@ -15,7 +15,7 @@
 
 // GETELEMENTBYID //
 // console.log(document.getElementById("exampleId"));
-var exampleVariable = document.getElementById("exampleId"));
+// var exampleVariable = doc ument.getElementById("exampleId");
 // console.log(exampleVariable);
 // exampleVariable.textContent = 'Hello';
 // exampleVariable.innerText = 'Goodbye';
@@ -24,4 +24,68 @@ var exampleVariable = document.getElementById("exampleId"));
 //INNERTEXT pays attention to styling where TEXTCONTENT does not
 // exampleVariable.innerHTML = '<h2>Hello</h2>';
 //INNERHTML puts variable INSIDE HTML element
-exampleVariable.style.borderBottom = 'solid 3px #000';
+// exampleVariable.style.borderBottom = "solid 3px orange";
+
+// GETELEMENTBYCLASSNAME //
+// var items = document.getElementsByClassName("exampleClass");
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = "hello";
+// items[1].style.fontWeight = "bold";
+// items[1].style.backgroundColor = "yellow";
+
+// // Gives error
+// // items.style.backgroundColor = "#f4f4f4";
+
+// for (var i = 0; i < items.length; i++) {
+//     items[i].style.backgroundColor = "#f4f4f4";
+// }
+
+// GETELEMENTBYTAGNAME //
+// var li = document.getElementsByTagName("li");
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = "hello";
+// li[1].style.fontWeight = "bold";
+// li[1].style.backgroundColor = "yellow";
+
+// // Gives error
+// // items.style.backgroundColor = "#f4f4f4";
+
+// for (var i = 0; i < li.length; i++) {
+//     li[i].style.backgroundColor = "#f4f4f4";
+// }
+
+// QUERYSELECTOR //
+// var header = document.querySelector("#exampleId");
+// header.style.borderBottom = "solid 4px orange";
+
+// var input = document.querySelector("input");
+// input.value = "Hello World";
+
+// var submit = document.querySelector('input[type = "submit"]');
+// submit.value = "SEND";
+
+// var item = document.querySelector(".list-group-item");
+// item.style.color = "red";
+
+// var lastItem = document.querySelector(".list-group-item:last-child");
+// lastItem.style.color = "blue";
+
+// var secondItem = document.querySelector(".list-group-item:nth-child(2)");
+// secondItem.style.color = "coral";
+
+// QUERYSELECTORALL //
+var titles = document.querySelectorAll(".title");
+
+console.log(titles);
+titles[0].textContent = "Hello";
+
+// alternating
+var odd = document.querySelectorAll("li:nth-child(odd)");
+var even = document.querySelectorAll("li:nth-child(even)");
+
+for (var i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = "#f4f4f4";
+    even[i].style.backgroundColor = "#ccc";
+}
