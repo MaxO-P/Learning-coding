5aa64dce52a6788a1136ce31dfd3ee01242ed48d
Mistakd <56619664+RealMistakd@users.noreply.github.com>
2021-09-29 08:53:10 -0400

    update

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,27 @@
-function
+// EXAMINE DOCUMENT OBJECT //
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 123;
+// console.log(document.doctype);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = "Hello";
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// GETELEMENTBYID //
+// console.log(document.getElementById("exampleId"));
+var exampleVariable = document.getElementById("exampleId"));
+// console.log(exampleVariable);
+// exampleVariable.textContent = 'Hello';
+// exampleVariable.innerText = 'Goodbye';
+// console.log(exampleVariable.innerText);
+// console.log(exampleVariable.textContent);
+//INNERTEXT pays attention to styling where TEXTCONTENT does not
+// exampleVariable.innerHTML = '<h2>Hello</h2>';
+//INNERHTML puts variable INSIDE HTML element
+exampleVariable.style.borderBottom = 'solid 3px #000';
