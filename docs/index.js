d1dd6be91282713b91bb662354e8ff4ca125f29e
Mistakd <56619664+RealMistakd@users.noreply.github.com>
2021-10-18 09:34:49 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -76,16 +76,88 @@
 // secondItem.style.color = "coral";
 
 // QUERYSELECTORALL //
-var titles = document.querySelectorAll(".title");
+// var titles = document.querySelectorAll(".title");
 
-console.log(titles);
-titles[0].textContent = "Hello";
+// console.log(titles);
+// titles[0].textContent = "Hello";
 
-// alternating
-var odd = document.querySelectorAll("li:nth-child(odd)");
-var even = document.querySelectorAll("li:nth-child(even)");
+// // alternating
+// var odd = document.querySelectorAll("li:nth-child(odd)");
+// var even = document.querySelectorAll("li:nth-child(even)");
 
-for (var i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor = "#f4f4f4";
-    even[i].style.backgroundColor = "#ccc";
-}
+// for (var i = 0; i < odd.length; i++) {
+//     odd[i].style.backgroundColor = "#f4f4f4";
+//     even[i].style.backgroundColor = "#ccc";
+// }
+
+// TRAVERSING THE DOM //
+var itemlist = document.querySelector("#items");
+//parentNode
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor = "#f4f4f4";
+// console.log(itemlist.parentNode.parentNode);
+// you can chain .parentNode
+
+//parentElement
+// console.log(itemlist.parentElement);
+// itemlist.parentElement.style.backgroundColor = "#f4f4f4";
+// console.log(itemlist.parentElement.parentElement.parentElement);
+
+//childNodes
+// console.log(itemlist.childNodes);
+
+// console.log(itemlist.children);
+// console.log(itemlist.children[1]);
+// itemlist.children[1].style.backgroundColor = "yellow";
+
+// //firstChild
+// console.log(itemlist.firstChild);
+// //firstElementChild
+// console.log(itemlist.firstElementChild);
+// itemlist.firstElementChild.textContent = "Hello";
+
+// //lastChild
+// console.log(itemlist.lastChild);
+// //lastElementChild
+// console.log(itemlist.lastElementChild);
+// itemlist.lastElementChild.textContent = "Hello";
+
+//nextSibling
+// console.log(itemlist.nextSibling);
+// //nextElementSibling
+// console.log(itemlist.nextElementSibling);
+
+// //previousSibling
+// console.log(itemlist.previousSibling);
+// //previousElementSibling
+// console.log(itemlist.previousElementSibling);
+// itemlist.previousElementSibling.style.color = "green";
+
+//createElement
+
+//Create a div
+var newDiv = document.createElement("div");
+
+// Add class
+newDiv.className = "hello";
+
+//Add id
+newDiv.id = "hello 1";
+
+//Add atrr
+newDiv.setAttribute("title", "Hello div");
+
+// Create text node
+var newDivText = document.createTextNode("Hello world");
+
+//Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
+
+console.log(newDiv);
+
+newDiv.style.fontSize = "30px";
+
+container.insertBefore(newDiv, h1);
